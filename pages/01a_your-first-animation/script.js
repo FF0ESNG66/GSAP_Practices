49a8bfbaf4c2ffcdd19e57f6e9f3774ed6067fbe
SNG66-ReadyChatAI <johny.gutierrez@readychatai.com>
2026-01-30 20:30:29 -0400

    Tetsing with gsap methods

--- pages/01a_your-first-animation/script.js
+++ pages/01a_your-first-animation/script.js
@@ -9,13 +9,5 @@ gsap.to(".card", {
             repeat: -1, // This mean that this will be reapated indefinitely
             yoyo: true,
         })
-
-        gsap.to(".card", {
-            boxShadow: "0 5px 90px 10px rgba(29, 209, 161, 0.6)",
-            repeat: -1,
-            yoyo: true,
-            duration: 1,
-            delay: 0.05,
-        });
     },
 });
---
+++ pages/01b_challenge/script.js
@@ -0,0 +1,21 @@
+gsap.to(".card", {
+    opacity: 1,
+    scale: 1,
+    duration: 1, // seconds
+    atComplete: () => {
+        gsap.to(".card", {
+            y: -20,
+            duration: 0.5,
+            repeat: -1, // This mean that this will be reapated indefinitely
+            yoyo: true,
+        })
+
+        gsap.to(".card", {
+            boxShadow: "0px 0px 90px 10px rgba(29, 209, 161, 0.6)",
+            repeat: -1,
+            yoyo: true,
+            duration: 1,
+            delay: 0.05,
+        });
+    },
+});
